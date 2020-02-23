4248e10a0ca4f084aaeafe2099d2cbb44f27fb86
Megan <mcoope13@gmail.com>
2020-02-23 17:43:39 -0500

    step 4

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-var magic8Ball = {};
+$(document).ready(function(){
+    
+    var magic8Ball = {};
+    
+    magic8Ball.listOfAnswers = ["It is certain.","It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Dont count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."];
+    
+    $("#answer").hide();
+    
+    magic8Ball.askQuestion = function(question){
+           $("#answer").fadeIn(4000);
+        var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+ 
+        $("#answer").text( answer );
+ 
+        console.log(question);
+        console.log(answer);
+    };
+ 
+    $("#answer").hide();
+    
+        var onClick = function() {
+        var question = prompt("ASK A YES/NO QUESTION!");
+        magic8Ball.askQuestion(question);
+ };
+   
 
-magic8Ball.listofAnswers = ["It is certain.","It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Dont count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."];
-magic8Ball.question;
-// define the method
-magic8Ball.askQuestion = function(question) {
- 
-     // create a random number
-     var randomNumber = Math.random();
- 
-     // make the random number between 0 and the number of items in your array
-     var randomNumberForListOfAnswers = randomNumber * this.listofAnswers.length;
- 
-     // round down the number to be a whole number
-     var randomIndex = Math.floor(randomNumberForListOfAnswers);
- 
-     // use that number to index a random number from the answers array
-     var answer = this.listofAnswers[randomIndex];
- 
-     console.log(question);
-     console.log(answer);
-};
-magic8Ball.askQuestion("Will I ever learn Javascript?");
+  $("#questionButton").click( onClick );
+    
+});
